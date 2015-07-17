0c9799fdf4d62f1c643cac776db814e39921ae5e
Randy Syring <randy@thesyrings.us>
2015-07-16 22:11:36 -0400

    ok, trying again

--- appveyor/mssql-server-setup.ps1
+++ appveyor/mssql-server-setup.ps1
@@ -1,7 +1,6 @@
 [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
 [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
 
-
 # Source: http://geekswithblogs.net/TedStatham/archive/2014/06/13/setting-the-ports-for-a-named-sql-server-instance-using.aspx
 # Set the $instanceName value below to the name of the instance you
 # want to configure a static port for. This could conceivably be
@@ -15,6 +14,14 @@ $wmi = New-Object ($smo + 'Wmi.ManagedComputer')
 $uri = "ManagedComputer[@Name='$serverName']/ServerInstance[@Name='$instanceName']/ServerProtocol[@Name='Tcp']"
 $Tcp = $wmi.GetSmoObject($uri)
 $Tcp.IsEnabled = $true
+$TCP.alter()
+
+# Configure IPAll setting so SQL Server listens on 1433 on all IP address and doesn't use dynamic ports
+$IPAllProps = $wmi.GetSmoObject($uri + "/IPAddress[@Name='IPAll']").IPAddressProperties
+$IPAllProps["TcpDynamicPorts"].Value = ""
+$IPAllProps["TcpPort"].Value = "1433"
+
+# apply changes
 $TCP.alter()
 
 # Start services
